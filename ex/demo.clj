ae374ff72b0aba79585dbe5bfff8023fbf0c3b03
William D. Lipe <atmcsld@gmail.com>
2009-03-28 12:29:56 -0500

    Updated demo.clj to work properly with all changes

--- ex/demo.clj
+++ ex/demo.clj
@@ -10,40 +10,40 @@
 
 ; an example of defining one track
 (deftrack unowen {:type [:note]}
-  [ d       a      e         a     ]
+  [ d        a+    e         a     ]
   [ f       (g a)  g         b     ]
-  [ (d5 a4) (e5 f) (e (f e)) (d c) ]
-  [ (a4 c5) (g4 a) f         *     ])
+  [ (d  a)  (e+ f) (e (f e)) (d c) ]
+  [ (a  c)  (g  a) f         *     ])
 
 
 ; an example of defining multiple tracks
 (deftrack
-  dm1 dm2 dm3 {:type [:note] :bpn 2 :oct 3}
+  dm1 dm2 dm3 {:type [:note :rest-zero] :bpn 2 :oct 4}
 
-  dm1 [ (a c4)  e        (d g)  e        ]
+  dm1 [ (a c)  e        (d g)  e        ]
   dm2 [  .     (. c * *)  *    (* c * *) ]
   dm3 [  .     (.   e  )  *    (*   e  ) ]
 
-  dm1 [ (d a)   e        b     c5        ]
-  dm2 [  *     (* e * *) b2    a         ]
+  dm1 [ (d a+)  e        b+    c         ]
+  dm2 [  *     (* e * *) b     a         ]
   dm3 [  *     (*   c  ) .     .         ])
 
 
-(deffilter guitar [(freq)]
+(deffilter guitar [freq]
   (if (= freq 0)
-    (constantly 0)
-    (karplus-strong (white-noise freq) freq 0.98)))
+    (repeat 0)
+    (karplus-strong (white-noise) freq 0.98)))
 
 
 (deffilter deep-mountain []
   (mix
-         (do-track guitar dm1)
-   (gain (do-track guitar dm2) 0.25)
-   (gain (do-track guitar dm3) 0.25)))
+         (with-track [dm1] (guitar dm1))
+   (gain (with-track [dm2] (guitar dm2)) 0.25)
+   (gain (with-track [dm3] (guitar dm3)) 0.25)))
 
 
-(time (render ["unowen.wav" (beats 16)]
-  (do-track guitar (transpose -12 unowen))))
+(render ["unowen.wav" (beats 16)]
+  (with-track [unowen] (guitar unowen)))
 
-(time (render ["deep-mountain.wav" (beats 16)]
-  (deep-mountain)))
+(render ["deep-mountain.wav" (beats 16)]
+  (deep-mountain))
